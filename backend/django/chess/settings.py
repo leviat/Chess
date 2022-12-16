719a997602e7e7f0c7d6143a4b1740f2a8abf740
Tuan Nguyen <ngoc.tuan.nguyen@rwth-aachen.de>
2022-12-16 13:41:54 +0100

    generate and hide new django secret key

--- backend/django/chess/settings.py
+++ backend/django/chess/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-3)w*p$jhs!bajgyp^^jk77=d9@%c9u)8s)wy6k5d*$x9em4nps"
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
